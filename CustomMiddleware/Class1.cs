3c1d2b5b43d7a65d0893e057705beee6f717e4c5
Oleksii Lukavnov <lukavnev@gmail.com>
2023-06-01 23:52:24 +0300

    Added custom middleware

---
+++ CustomMiddleware/Class1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CustomMiddleware
+{
+    public class ConsoleMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ConsoleMiddleware(RequestDelegate next) => this.next = next;
+
+        public Task Invoke(HttpContext ctx)
+        {
+            Console.WriteLine("Got request in class middleware");
+            return this.next(ctx);
+        }
+    }
+
+    public class RedirectingMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public RedirectingMiddleware(RequestDelegate next) => this.next = next;
+
+        public Task Invoke(HttpContext ctx)
+        {
+            switch (ctx.Request.Path.Value?.TrimEnd('/'))
+            {
+                case "/oldpath":
+                    {
+                        ctx.Response.Redirect("/newpath", permanent: true);
+                        return Task.CompletedTask;
+                    }
+                default:
+                    return this.next(ctx);
+            }
+        }
+    }
+}
